60af1cf4723e430d1f7475f650f548fc1a183f6a
e <13767927306@163.com>
2020-05-18 15:41:55 +0800

    实验初步完成

--- data_run/ticdata2000/run.py
+++ data_run/ticdata2000/run.py
@@ -4,3 +4,60 @@ from reduct import Reduct
 from multiReduct import MReduct
 from DealData import DealData
 from Result import Result
+import time
+
+"""
+
+ticdata2000:
+
+5822行,85个属性
+0: 5474, 1: 348
+"""
+
+list_name = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 'A14', 'A15', 'A16',
+             'A17', 'A18', 'A19', 'A20', 'A21', 'A22', 'A23', 'A24', 'A25', 'A26', 'A27', 'A28', 'A29', 'A30', 'A31',
+             'A32', 'A33', 'A34', 'A35', 'A36', 'A37', 'A38', 'A39', 'A40', 'A41', 'A42', 'A43', 'A44', 'A45', 'A46',
+             'A47', 'A48', 'A49', 'A50', 'A51', 'A52', 'A53', 'A54', 'A55', 'A56', 'A57', 'A58', 'A59', 'A60', 'A61',
+             'A62', 'A63', 'A64', 'A65', 'A66', 'A67', 'A68', 'A69', 'A70', 'A71', 'A72', 'A73', 'A74', 'A75', 'A76',
+             'A77', 'A78', 'A79', 'A80', 'A81', 'A82', 'A83', 'A84', 'A85', 'D']
+
+pre_path = "../../data_set/ticdata2000/"
+
+
+def deal():
+    path = pre_path + "ticdata2000.txt"
+    separator = " "
+    d = DealData(path, separator, list_name)
+    d.standard_data()
+
+
+def split():
+    train_path = pre_path + "ticdata2000.csv"
+    rate = 0.8
+    attributes_num = list()
+    Result(train_path, rate, attributes_num)
+
+
+def result():
+    train_path = pre_path + "ticdata2000_train.csv"
+    t1 = time.time()
+    r = Reduct(train_path)
+    r.gain_reduct()
+    print("r:", time.time() - t1)
+    print(r.reduct_list)
+    """"""
+    t2 = time.time()
+    rm = MReduct(train_path, 3)
+    rm.gain_multi_reduct()
+    print("rm:", time.time() - t2)
+    print(rm.reduct_all_list)
+
+
+"""
+
+"""
+
+if __name__ == "__main__":
+    # deal()
+    # split()
+    result()
